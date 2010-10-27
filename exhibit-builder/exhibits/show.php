4d27993b8210c7b0b5a99294820b888f14e9404a
Ken Albers <ken.albers@gmail.com>
2010-10-27 19:41:03 +0000

    Added exhibit views.

---
+++ exhibit-builder/exhibits/show.php
@@ -0,0 +1,23 @@
+<?php head(array('title' => html_escape(exhibit('title') . ' : '. exhibit_page('title')), 'bodyid'=>'exhibit','bodyclass'=>'show')); ?>
+<div id="primary">
+	<h1><?php echo link_to_exhibit(); ?></h1>
+
+
+
+	<h2><?php echo exhibit_page('title'); ?></h2>
+	
+	<?php exhibit_builder_render_exhibit_page(); ?>
+	
+	<div id="exhibit-page-navigation">
+	   	<?php echo exhibit_builder_link_to_previous_exhibit_page(); ?>
+    	<?php echo exhibit_builder_link_to_next_exhibit_page(); ?>
+	</div>
+</div>	
+
+<div id="secondary">
+<div id="nav-container">
+	<?php echo exhibit_builder_section_nav();?>
+	<?php echo exhibit_builder_page_nav();?>
+</div>
+</div>
+<?php foot(); ?>
