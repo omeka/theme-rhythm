c0b7da7fe385f89431636e2f283bb58b7457fc7c
Jeremy Boggs <jeremy@clioweb.org>
2010-10-28 17:09:22 +0000

    Updates custom theme helpers for showing item metadata to check if Dublin Core field exists.

--- custom.php
+++ custom.php
@@ -133,10 +133,12 @@ function rhythm_show_item_metadata(array $options = array(), $item = null)
 	    $dcFields = explode(',', $dcFieldsList);
 	    foreach ($dcFields as $field) {
 	        $field = trim($field);
-	        if ($fieldValue = item('Dublin Core', $field)) {
-	            $html .= '<h3>'.$field.'</h3>';
-	            $html .= $fieldValue;
-	        }
+	        if (element_exists('Dublin Core', $field)) {
+    	        if ($fieldValue = item('Dublin Core', $field)) {
+    	            $html .= '<h3>'.$field.'</h3>';
+    	            $html .= $fieldValue;
+    	        }
+    	    }
 	    }
 	    $html .= show_item_metadata(array('show_element_sets' => array('Item Type Metadata')));
 	    return $html;
