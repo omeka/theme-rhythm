b11aa61569b491241b9b79ccf1ecee5375b703d6
John Flatness <john@zerocrates.org>
2011-12-15 11:39:42 -0500

    Update common/header.php to track core version.

--- common/header.php
+++ common/header.php
@@ -1,50 +1,50 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html class="<?php echo get_theme_option('Style Sheet'); ?>" xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="<?php echo get_html_lang(); ?>" class="<?php echo get_theme_option('Style Sheet'); ?>">
 <head>
-<title><?php echo settings('site_title'); echo $title ? ' | ' . $title : ''; ?></title>
+    <meta charset="utf-8">
+    <?php if ( $description = settings('description')): ?>
+    <meta name="description" content="<?php echo $description; ?>" />
+    <?php endif; ?>
 
-<!-- Meta -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="description" content="<?php echo settings('description'); ?>" />
+    <title><?php echo settings('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?></title>
 
-<?php echo auto_discovery_link_tag(); ?>
+    <?php echo auto_discovery_link_tags(); ?>
 
-<!-- Plugin Stuff -->
-<?php plugin_header(); ?>
+    <!-- Plugin Stuff -->
+    <?php plugin_header(); ?>
 
-<!-- Stylesheets -->
-<?php 
-queue_css('style');
-display_css(); 
-?>
+    <!-- Stylesheets -->
+    <?php
+    queue_css('style');
+    display_css();
+    ?>
 
-<!-- JavaScripts -->
-<?php display_js(); ?>
-
+    <!-- JavaScripts -->
+    <?php display_js(); ?>
 </head>
-<body<?php echo $bodyid ? ' id="'.$bodyid.'"' : ''; ?><?php echo $bodyclass ? ' class="'.$bodyclass.'"' : ''; ?>>
+<?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
+    <?php plugin_body(); ?>
 
 <div id="wrap">
-  <div id="header">
+    <div id="header">
+	    <?php plugin_page_header(); ?>
+        <div id="site-title">    
+            <p class="sitetitle"><?php echo link_to_home_page(custom_display_logo()); ?></p>
+            <?php echo rhythm_display_tagline(); ?>
+        </div> <!-- end #site-title -->
 	
-	<div id="site-title">    
-		<p class="sitetitle"><?php echo link_to_home_page(custom_display_logo()); ?></p>
-		<?php echo rhythm_display_tagline(); ?>
-	</div> <!-- end #site-title -->
+        <div id="searchwrap">
+            <?php echo simple_search(); ?>
+            <?php echo link_to_advanced_search(); ?>
+    	</div> <!-- end #searchwrap -->
 	
-    <div id="searchwrap">
-     	<?php echo simple_search(); ?>
-		<?php echo link_to_advanced_search(); ?>
-	</div> <!-- end #searchwrap -->
-	
-  </div>  <!-- end #header -->
+    </div>  <!-- end #header -->
   
- <div id="primary-nav">
-     <ul class="navigation">
-		<?php echo custom_public_nav_header(); ?>
-     	
-    </ul>
-  </div>  <!-- end #primary-nav -->
-
-<div id="content">
+    <div id="primary-nav">
+        <ul class="navigation">
+            <?php echo custom_public_nav_header(); ?>
+        </ul>
+    </div>  <!-- end #primary-nav -->
+    
+    <div id="content">
+        <?php plugin_page_content(); ?>
