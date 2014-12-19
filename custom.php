35eb69804adc9a7616c2d3fa636de4f280ea7ec6
Kim Nguyen <design@kimisgold.com>
2014-12-19 15:00:10 -0500

    Implement exhibit builder nested list-style navigation for pages.

--- custom.php
+++ custom.php
@@ -43,3 +43,37 @@ function rhythm_display_date_added($format = 'F j, Y', $item = null) {
     $dateAdded = metadata($item, 'added');
     return date($format, strtotime($dateAdded));   
 }
+
+function rhythm_exhibit_builder_page_nav($exhibitPage = null, $currentPageId)
+{
+    if (!$exhibitPage) {
+        $exhibitPage = get_current_record('exhibit_page');
+    }
+
+    $parents = array();
+    $currentPage = get_record_by_id('Exhibit Page', $currentPageId);
+    while ($currentPage->parent_id) {        
+        $currentPage = $currentPage->getParent();
+        array_unshift($parents, $currentPage->id);
+    }
+    
+    $class  = '';
+    $class .= ($exhibitPage->id == $currentPageId) ? 'current' : '';
+    $parent = (array_search($exhibitPage->id, $parents) !== false) ? ' parent' : '';
+
+    $html = '<li class="' . $class . $parent . '">'
+          . '<a href="' . exhibit_builder_exhibit_uri(get_current_record('exhibit'), $exhibitPage) . '">'
+          . metadata($exhibitPage, 'title') .'</a>';
+
+    $children = $exhibitPage->getChildPages();
+    if ($children) {
+        $html .= '<ul>';
+        foreach ($children as $child) {
+            $html .= rhythm_exhibit_builder_page_nav($child, $currentPageId);
+            release_object($child);
+        }
+        $html .= '</ul>';
+    }
+    $html .= '</li>';
+    return $html;
+}
