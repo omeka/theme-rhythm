525336d89dcb0eb1012be11801392863a670c5ac
Kim Nguyen <design@kimisgold.com>
2024-11-15 15:16:41 -0500

    Remove unused custom function.

--- custom.php
+++ custom.php
@@ -5,29 +5,6 @@
 // possible. Ideally, you should namespace these with your theme name.
 
 
-function rhythm_display_random_featured_item_squarethumb()
-{
-    $featuredItems = get_random_featured_items(1);
-    $featuredItem = $featuredItems[0];
-
-    $html = '<h2>Featured <span class="type-featured">Item</span></h2>';
-    if ($featuredItem) {
-        set_current_record('item', $featuredItem); // Needed for transparent access of item metadata.
-
-       if (metadata('item', 'has thumbnail')) {
-           $html .= link_to_item(item_square_thumbnail(), array('class'=>'image'));
-       }
-       // Grab the 1st Dublin Core description field (first 150 characters)
-       $itemDescription = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>150));
-       $html .= '<p><span class="title">' . link_to_item() . '</span>&nbsp;&nbsp;&nbsp;' . $itemDescription . '</p>';
-    } else {
-       $html .= '<p>No featured items are available.</p>';
-    }
-
-    return $html;
-}
-
-
 function rhythm_display_tagline()
 {
     if ($tagline = get_theme_option('site_tagline')) {
